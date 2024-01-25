7f1a9d9747ea749d39c6ac113654e080249d0fd3
Jonas Ohlsson <jonas@vansinne.se>
2024-01-25 10:47:21 +0100

    return NaN instead of 'U' to simplify dealing with these responses

--- sensors.py
+++ sensors.py
@@ -2,28 +2,26 @@ import minimalmodbus
 from pymodbus.client import ModbusTcpClient, ModbusSerialClient
 from pymodbus.exceptions import ModbusException
 
-
 # minimalmodbus is nice to work with but lacks tcp support,
 # so we use it for serial but pymodbus for tcp
-# return 'U' for unknown values to be compatible with rrdtool
 class BlueVCount(minimalmodbus.Instrument):
     def get_vol(self):
         try:
             return self.read_float(1, byteorder=minimalmodbus.BYTEORDER_LITTLE)
         except (minimalmodbus.ModbusException):
-            return 'U'
+            return float('nan')
 
     def get_temp(self):
         try:
             return self.read_float(9, byteorder=minimalmodbus.BYTEORDER_LITTLE)
         except (minimalmodbus.ModbusException):
-            return 'U'
+            return float('nan')
 
     def get_pressure(self):
         try:
             return self.read_float(5, byteorder=minimalmodbus.BYTEORDER_LITTLE)
         except (minimalmodbus.ModbusException):
-            return 'U'
+            return float('nan')
 
 
 # return 'U' for unknown values to be compatible with rrdtool
@@ -39,7 +37,7 @@ class BlueVary(ModbusTcpClient):
             val = self.read_holding_registers(0, 2, slave=2)
             return self.convert_from_registers([val.registers[1], val.registers[0]], data_type=self.DATATYPE.FLOAT32)
         except:
-            return 'U'
+            return float('nan')
 
     def get_h2(self):
         '''Assume a H2 sensor on channel 2.'''
@@ -48,7 +46,7 @@ class BlueVary(ModbusTcpClient):
             val = self.read_holding_registers(0, 2, slave=3)
             return self.convert_from_registers([val.registers[1], val.registers[0]], data_type=self.DATATYPE.FLOAT32)
         except:
-            return 'U'
+            return float('nan')
 
     def get_humidity(self):
         '''Absolute humidity'''
@@ -57,7 +55,7 @@ class BlueVary(ModbusTcpClient):
             val = self.read_holding_registers(0, 2, slave=4)
             return self.convert_from_registers([val.registers[1], val.registers[0]], data_type=self.DATATYPE.FLOAT32)
         except:
-            return 'U'
+            return float('nan')
 
 #bluevcount = BlueVCount('/dev/tty.usbserial-AU05SI5H', slaveaddress=1)
 #bluevcount.serial.baudrate = 38400

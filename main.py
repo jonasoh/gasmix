f3bffb4bb1516c6b887283bb2e7144473a92c8d5
Jonas Ohlsson <jonas@vansinne.se>
2024-01-26 09:52:38 +0100

    choose first usb device and allow user to specify the device

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import time
 import argparse
 from collections import deque
@@ -9,9 +10,12 @@ import rrd
 from gas_switch import activate_rocker, cleanup
 from sensors import BlueVary, BlueVCount
 
+print('Gas logger and controller starting up.')
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode')
 parser.add_argument('-c', '--cycle-length', type=int, default=60, help='Set CO2/H" measuring cycle length (default: 60)')
+parser.add_argument('-d', '--device', type=str, default=None, help='Device entry for the USB to RS485 adapter (default: first /dev/ttyUSB* entry)')
 args = parser.parse_args()
 
 VERBOSE = args.verbose
@@ -31,6 +35,22 @@ except:
     db.rebuild_db()
 
 # connect to sensors
+if not args.device:
+    devs = []
+    with os.scandir('/dev') as d:
+        for entry in d:
+            if entry.name.startswith('ttyUSB'):
+                devs.append('/dev/' + entry.name)
+    if devs:
+        USB_DEV = sorted(devs)[0]
+    else:
+        raise IOError("No USB device found for RS485 communication.")
+else:
+    USB_DEV = args.device
+
+assert os.access(USB_DEV, mode=os.R_OK|os.W_OK), f"USB device ({USB_DEV}) not accessible."
+print(f'Using {USB_DEV} for serial communication.')
+
 bv = BlueVary('192.168.10.230')
 bcs = [BlueVCount('/dev/ttyUSB0', 1), 
        BlueVCount('/dev/ttyUSB0', 2), 
@@ -42,7 +62,6 @@ bcs[0].serial.stopbits = 2
 reactors = deque([0, 1, 2])
 
 try:
-    print('Gas logger and controller starting up.')
     while True:
         # outer loop: this is for measuring H2/CO2.
         # we need to use a relatively long time for this measurement (settable via --cycle-length)
@@ -60,20 +79,23 @@ try:
                 time.sleep(0.01)
             end_vols = get_vols(bcs)
 
-            rrd.record_data(flows=(flows := [(x - y) / ((time.monotonic() - loopstart) / 60) for x, y in zip(end_vols, init_vols)]), reactor=r, h2=(h2 := bv.get_h2()), co2=(co2 := bv.get_co2()))
+            rrd.record_data(flows=(flows := [(x - y) / ((time.monotonic() - loopstart) / 60) for x, y in zip(end_vols, init_vols)]), 
+                            reactor=r, h2=(h2 := bv.get_h2()), 
+                            co2=(co2 := bv.get_co2()))
             if (VERBOSE):
                 print(f"{flows=} {h2=} {co2=}")
 
             for cur_r in [0,1,2]:
-                db.queries.insert_sensordata(read_time=int(time.time()),
-                                    reactor=cur_r,
-                                    vol=flows[cur_r],
-                                    h2=h2 if cur_r == r else np.nan,
-                                    co2=co2 if cur_r == r else np.nan,
-                                    temp=bcs[cur_r].get_temp(),
-                                    pressure=bcs[cur_r].get_pressure(),
-                                    humidity=bv.get_humidity() if cur_r == r else np.nan,
-                                    comment='')
+                db.queries.insert_sensordata(
+                    read_time=int(time.time()),
+                    reactor=cur_r,
+                    vol=flows[cur_r],
+                    h2=h2 if cur_r == r else np.nan,
+                    co2=co2 if cur_r == r else np.nan,
+                    temp=bcs[cur_r].get_temp(),
+                    pressure=bcs[cur_r].get_pressure(),
+                    humidity=bv.get_humidity() if cur_r == r else np.nan,
+                    comment='')
 
 finally:
     cleanup() # clean up GPIO
